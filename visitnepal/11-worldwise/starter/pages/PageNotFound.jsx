287d656d1e2ff31321be7780a9f1016cdfe5448c
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-02 21:26:42 +0545

    applayout started

---
+++ visitnepal/11-worldwise/starter/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <PageNav/>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
+}
