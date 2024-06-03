09a849a648f552cda9aa43e2661df67ab55facac
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-03 23:16:01 +0545

    finaladded

--- visitnepal/src/components/CityItem.jsx
+++ visitnepal/src/components/CityItem.jsx
@@ -1,34 +1,40 @@
-import styles from './CityItem.module.css'
+import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./CityItem.module.css";
 
-const formatDate = (date) => {
-  try {
-    // Check if date is a valid date object
-    if (!(date instanceof Date)) {
-      throw new Error('Invalid date');
-    }
-    
-    return new Intl.DateTimeFormat("en", {
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-      weekday: "long",
-    }).format(date);
-  } catch (error) {
-    console.error(error);
-    return "Invalid Date";
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { currentCity, deleteCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
   }
-}
 
-function CityItem(city) {
-    const {cityName, emoji, date}= city;
-    return (
-        <li className={styles.cityItem}>
-            <span className={styles.emoji}>{emoji}</span>
-            <h3 className={styles.name}>{cityName}</h3>
-            <time className={styles.date}>{formatDate(date)}</time>
-          <button className={styles.deleteBtn}>&times;</button> 
-        </li>
-    )
+  return (
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
+      </Link>
+    </li>
+  );
 }
 
 export default CityItem;
