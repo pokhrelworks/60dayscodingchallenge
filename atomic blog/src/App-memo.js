c1cbd6c68d4ad97ec405cf3c96a8608b956b58d7
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-10 21:45:45 +0545

    memo

--- atomic blog/src/App-memo.js
+++ atomic blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -41,6 +41,11 @@ function App() {
     [isFakeDark]
   );
 
+  const archiveOptions = {
+    show: false,
+    title: "Post archive in addition to main posts",
+  };
+
   return (
     <section>
       <button
@@ -57,7 +62,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={false} />
+      <Archive archiveOptions={archiveOptions} />
       <Footer />
     </section>
   );
@@ -154,14 +159,14 @@ function List({ posts }) {
   );
 }
 
-function Archive({ show }) {
+const Archive = memo(function Archive({archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
@@ -175,7 +180,7 @@ function Archive({ show }) {
           {posts.map((post, i) => (
             <li key={i}>
               <p>
-                <strong>{post.title}:</strong> {post.body}
+                <strong>{archiveOptions.title}:</strong> {post.body}
               </p>
               {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
             </li>
@@ -184,7 +189,7 @@ function Archive({ show }) {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
