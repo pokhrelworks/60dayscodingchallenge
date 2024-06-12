11b601301c02fe55d24a5407b414ea493ccdca11
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-12 23:06:44 +0545

    addedcombinereducers

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//Importing whole file so that it would run in this program
+import './store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,19 +1,26 @@
-const initialState = {
+import { combineReducers, createStore } from "redux";
+
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoad":
+    case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
-    case "account/payLoad":
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -24,3 +31,94 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// Combining all the possible reducers available into a root reducer.
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+// Now it's time to include redux in our code base
+const store = createStore(rootReducer);
+
+// Working with the Action Creators
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(200, "Buy a car"));
+store.dispatch(payLoan());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+store.dispatch(createCustomer("Ankit", "54254d54f"));
+console.log(store.getState());
+
+// //Here we create the dispatch function with store
+// store.dispatch({ type: "account/deposit", payload: 500 });
+
+// // console.log(store.getState());
+// // console.log("Hey Redux");
+
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "buy a car" },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
