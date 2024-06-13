48d304bf669919e3999dc2e567f661d5cff3df45
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-13 21:07:30 +0545

    slicedone

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 //Importing whole file so that it would run in this program
-import './store'
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+store.dispatch({
+  type: "customer/createCustomer",
+  payload: { fullName: "Ankit", nationalID: "112" },
+});
+console.log(store.getState());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
